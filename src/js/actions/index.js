ebf5d649602a7267127d72d21c4686c904e7afe6
Ryan Spink <ryanspinktech@gmail.com>
2018-06-10 15:16:31 -0600

    added change user email
    changed userprofileinfo component name

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -4,6 +4,7 @@ import { LOGOUT } from "../constants/action-types";
 import { ADD_USER_CONVERSION } from "../constants/action-types";
 import { DELETE_CONVERSION } from "../constants/action-types";
 import { ADD_LOGIN_BUTTON_STATUS } from "../constants/action-types";
+import { CHANGE_USER_EMAIL } from "../constants/action-types";
 
 export const addUserData = userData => ({ type: ADD_USER_DATA, payload: userData });
 export const login = loggedIn => ({ type: LOGIN, payload: loggedIn});
@@ -11,3 +12,4 @@ export const logout = loggedOut => ({ type: LOGOUT, payload: loggedOut})
 export const addUserConversion = userConversion => ({ type: ADD_USER_CONVERSION, payload: userConversion });
 export const deleteConversion = conversion => ({ type: DELETE_CONVERSION, payload: conversion });
 export const addLoginButtonStatus = boolean => ({ type: ADD_LOGIN_BUTTON_STATUS, payload: boolean });
+export const changeUserEmail = email => ({ type: CHANGE_USER_EMAIL, payload: email });
--- src/js/components/UserProfile.js
+++ src/js/components/UserProfile.js
@@ -3,9 +3,11 @@ import { Grid } from 'semantic-ui-react';
 import TextToSpeechForm from './TextToSpeechForm';
 import ConversionList from './ConversionList';
 import Navbar from './Navbar';
+import UserProfileEmail from './UserProfileEmail'
 
 export const UserProfile = () => (
   <Grid>
     <Navbar/>
+    <UserProfileEmail/>
   </Grid>
 )
