872b3434718948010c680eb46ac8e2be7be5726c
Ryan Spink <ryanspinktech@gmail.com>
2018-09-20 13:22:28 -0600

    reconfiured for deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import index from "./js/index";
 import './semantic/dist/semantic.min.css';
+import React from "react";
+import { render } from "react-dom";
+import store from "./js/store/index";
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { Provider } from "react-redux";
+import App from "./js/components/App";
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
 
+
+render(
+  <Provider store={store}>
+    <Router>
+      <Route path="/:filter?" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById("app")
+);
+registerServiceWorker();
 
 console.log(`Started`);
 
